b7d53869db8b6d669bf2ced2a6be43d357949670
zlincon <linconsilva08@gmail.com>
2021-06-17 19:27:57 -0300

    Serialização customizada - concluded

--- src/main/java/br/com/zupacademy/lincon/EmailService.java
+++ src/main/java/br/com/zupacademy/lincon/EmailService.java
@@ -2,12 +2,14 @@ package br.com.zupacademy.lincon;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
+import java.util.Map;
+
 public class EmailService {
   public static void main(String[] args) throws InterruptedException {
     var emailService = new EmailService();
     try (var service = new KafkaService(EmailService.class.getSimpleName(),
         "ECOMMERCE_SEND_EMAIL",
-        emailService::parse, Email.class)) {
+        emailService::parse, Email.class, Map.of())) {
       service.run();
     }
   }
@@ -17,8 +19,8 @@ public class EmailService {
     System.out.println("Sending email");
     System.out.println("Thank you for your order! We are processing it!");
     System.out.println(record.key());
-    System.out.println(record.partition());
     System.out.println(record.value());
+    System.out.println(record.partition());
     System.out.println(record.offset());
     Thread.sleep(1000);
     System.out.println("Email sent");
--- src/main/java/br/com/zupacademy/lincon/FraudDetectorService.java
+++ src/main/java/br/com/zupacademy/lincon/FraudDetectorService.java
@@ -2,13 +2,16 @@ package br.com.zupacademy.lincon;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
+import java.util.HashMap;
+
 public class FraudDetectorService {
   public static void main(String[] args) throws InterruptedException {
     var fraudService = new FraudDetectorService();
     try (var service =
              new KafkaService<>(FraudDetectorService.class.getSimpleName(),
                  "ECOMMERCE_NEW_ORDER",
-                 fraudService::parse, Order.class)) {
+                 fraudService::parse, Order.class,
+                 new HashMap<>())) {
       service.run();
     }
   }
@@ -18,8 +21,8 @@ public class FraudDetectorService {
     System.out.println("Processing new order, checking for " +
         "fraud");
     System.out.println(record.key());
-    System.out.println(record.partition());
     System.out.println(record.value());
+    System.out.println(record.partition());
     System.out.println(record.offset());
     Thread.sleep(5000);
     System.out.println("Order processed");
